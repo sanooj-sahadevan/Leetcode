635b8f47c54e2c8243a69700daa8e123ff542eeb
sanooj <146158357+sanooj-sahadevan@users.noreply.github.com>
2024-02-23 09:39:59 +0530

    added

---
+++ leetcode905.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortArrayByParity = function(nums) {
+    
+    let  x  = []
+    let  y  = []
+    
+    
+    nums.sort()
+    for(i=0;i<nums.length;i++){
+        if(nums[i]%2==0){
+      x.push(nums[i])
+        }else{
+            y.push(nums[i])
+        }
+    }
+    let a = [...x,...y]
+    return a
+    console.log(nums)
+    console.log(x)
+    console.log(y)
+    console.log(a)
+    };
