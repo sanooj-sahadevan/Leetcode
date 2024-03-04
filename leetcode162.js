0ea825f95e5a85886a7f5c44899516bc7fca39d3
sanooj <146158357+sanooj-sahadevan@users.noreply.github.com>
2024-03-04 12:23:20 +0530

    add

---
+++ leetcode162.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findPeakElement = function(nums) {
+    
+    let l = Math.max(...nums)
+    
+    for(i=0;i<nums.length;i++){
+        if(l===nums[i]){
+            return i
+        }
+    }
+    
+    };
