f159e8c97f6c6c8a8e1883269751e613068f863e
sanooj <146158357+sanooj-sahadevan@users.noreply.github.com>
2024-03-19 09:27:05 +0530

    add

---
+++ 1450.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} startTime
+ * @param {number[]} endTime
+ * @param {number} queryTime
+ * @return {number}
+ */
+var busyStudent = function (startTime, endTime, queryTime) {
+
+    var ctr = 0;
+    for (let i = 0; i < startTime.length; i++) {
+        if (endTime[i] >= queryTime && startTime[i] <= queryTime) {
+            ctr++;
+        }
+    }
+    return ctr;
+};
