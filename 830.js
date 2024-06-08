3dfd57e80190b077566b07b55b04041180386946
sanooj <146158357+sanooj-sahadevan@users.noreply.github.com>
2024-06-08 10:24:10 +0530

    add

---
+++ 830.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s
+ * @return {number[][]}
+ */
+var largeGroupPositions = function (s) {
+
+    let result = []
+
+    for (let i = 0; i < s.length; i++) {
+
+        let startIndex = i
+        let count = 0
+        let end = 0
+
+        while (s[i] == s[i + 1]) {
+            count++
+            end = i + 1
+            i++
+
+        }
+        if (count >= 2) {
+
+            result.push([startIndex, end])
+        }
+    }
+
+    return result
+};
--- 908.js
+++ 908.js
@@ -7,11 +7,7 @@ let nums = [1, 3, 6],
 // Output: 0
 // Explanation: Change nums to be [4, 4, 4]. The score is max(nums) - min(nums) = 4 - 4 = 0.
 
-nums.sort((a, b) => b - a);
-console.log(nums);
 
-let range = nums[0] - k - (nums[nums.length - 1] + k);
 
-range>0 ? range :0
 
 console.log(range);
