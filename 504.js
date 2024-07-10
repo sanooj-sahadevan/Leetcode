f5af49d28c957fd08ee86effff55f8b2274d0334
sanooj <146158357+sanooj-sahadevan@users.noreply.github.com>
2024-07-10 10:12:24 +0530

    add

---
+++ 504.js
@@ -0,0 +1,20 @@
+const num = 100;
+// Output: "202"
+// Example 2:
+
+// Input: num = -7
+// Output: "-10"
+return toStringfy(7)
+// let x = [];
+// let res = 0,
+//   div = 0;
+// for (let i = 0; i < num; i++) {
+//   res = num / 7;
+//   div = num % 7;
+//   console.log(div);
+//   x.push(div);
+//   x.push(Math.floor(div / 7));
+//   break;
+// }
+
+// console.log(x);
