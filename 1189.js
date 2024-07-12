83857d9e2e883053a19f3232ec22c5df9a34fd91
sanooj <146158357+sanooj-sahadevan@users.noreply.github.com>
2024-07-12 09:57:22 +0530

    add

---
+++ 1189.js
@@ -0,0 +1,30 @@
+let text = "nlaebolko";
+// Output: 1
+// Example 2:
+
+// Input: text = "loonbalxballpoon"
+// Output: 2
+// Example 3:
+
+// Input: text = "leetcode"
+// Output: 0
+// not complete
+                                                                       // not complete
+let bal = ["b", "a", "l", "o", "o", "n"];     
+text = text.split("");
+console.log(text);
+
+let res = [];
+for (let i = 0; i < text.length; i++) {
+  if (bal.includes(text[i])) {
+    res.push(text[i]);
+  }
+}
+
+
+for(i=0;i<res.length;i++){
+    if(bal.includes(res[i])){
+
+    }
+}
+console.log(res);
