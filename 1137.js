d627747afe216bc75f19c83edf9ff014119d8dfe
sanooj <146158357+sanooj-sahadevan@users.noreply.github.com>
2024-07-13 09:31:11 +0530

    add

--- 1137.js
+++ 1137.js
@@ -1,17 +1,3 @@
-// let t = [0, 1, 1];
-//     if (n === 0) return 0;
-//     if (n === 1 || n === 2) return t[n];
-//     for (let i = 3; i <= n; i++) {
-//         t[i] = t[i - 1] + t[i - 2] + t[i - 3];
-//     }
-//     return t[n];
-
-// T0 = 0, T1 = 1, T2 = 1, and Tn+3 = Tn + Tn+1 + Tn+2 for n >= 0.
-
-// Given n, return the value of Tn.
-
-// Example 1:
-
 const n = 4;
 // Output: 4
 // Explanation:
