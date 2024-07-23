86ca73e5ebaa42b66a3181fdaf65523a0a12bf35
sanooj <146158357+sanooj-sahadevan@users.noreply.github.com>
2024-07-23 10:17:49 +0530

    add

--- 1636.js
+++ 1636.js
@@ -1,9 +1,9 @@
-const nums = [1, 1, 2, 2, 2, 3];
+// const nums = [1, 1, 2, 2, 2, 3];
 // Output: [3,1,1,2,2,2]
 // Explanation: '3' has a frequency of 1, '1' has a frequency of 2, and '2' has a frequency of 3.
 // Example 2:
 
-// Input: nums = [2,3,1,3,2]
+const nums = [2, 3, 1, 3, 2];
 // Output: [1,3,3,2,2]
 // Explanation: '2' and '3' both have a frequency of 2, so they are sorted in decreasing order.
 // Example 3:
@@ -22,14 +22,15 @@ for (let i = 0; i < nums.length; i++) {
   f[nums[i]]++;
 }
 
-// for (let key in f) {
-//   if (f[key] < f[key + 1]) {
-//     let temp = f[key];
-//     f[key] = f[key + 1];
-//     f[key + 1] = temp;
-//   }
+let entries = Object.entries(f);
 
-// }
+entries.sort((a, b) => a[1] - b[1]);
+console.log(entries);
 
-
-console.log(f);
+for (let [key, value] of entries) {
+  for (let i = 0; i < value; i++) {
+    x.push(Number(key));
+  }
+}
+// console.log(f);
+// console.log(x);
