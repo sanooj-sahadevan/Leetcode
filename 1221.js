9f1759aa14d419722dcd3eff334fed64cd3136de
sanooj <146158357+sanooj-sahadevan@users.noreply.github.com>
2024-07-27 10:06:09 +0530

    add

--- 1221.js
+++ 1221.js
@@ -1,27 +1,15 @@
-// const s = "RLRRLLRLRL";
-// Output: 4;
-// // Explanation: s can be split into "RL", "RRLL", "RL", "RL", each substring contains same number of 'L' and 'R'
+let s = "RLRRLLRLRL"
+// Output: 4
+// Explanation: s can be split into "RL", "RRLL", "RL", "RL", each substring contains same number of 'L' and 'R'.
+// Example 2:
 
-// let x = [];
+// Input: s = "RLRRRLLRLL"
+// Output: 2
+// Explanation: s can be split into "RL", "RRRLLRLL", each substring contains same number of 'L' and 'R'.
+// Note that s cannot be split into "RL", "RR", "RL", "LR", "LL", because the 2nd and 5th substrings are not balanced.
+// Example 3:
 
+// Input: s = "LLLLRRRR"
+// Output: 1
+// Explanation: s can be split into "LLLLRRRR".
 
-// for(i=0;i<s.length;i++){
-
-//     if(s[i]!==s[i+1]){
-//         s.split(' ')
-//     }
-
-// }
-// console.log(s);
-
-// let res = ''
-// for(let i = 0; i < s.length; i++){    
-//     if(i % 2 !== 0){
-//       res += String.fromCharCode(s[i - 1].charCodeAt() + parseInt(s[i]))
-//     } else{
-//         res += s[i]
-//     }
-    
-// }
-// return res;
-// };
