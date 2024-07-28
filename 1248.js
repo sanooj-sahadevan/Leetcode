9a121f1712e982d8631f615d0af003a47e7bc5a6
sanooj <146158357+sanooj-sahadevan@users.noreply.github.com>
2024-07-28 15:47:45 +0530

    add

--- 1248.js
+++ 1248.js
@@ -1,7 +1,6 @@
-const nums = [1,1,2,1,1],
+const nums = [1, 1, 2, 1, 1],
   k = 3;
-let ctr = 0,
-  res = [];
+
 // Output: 2
 // Explanation: The only sub-arrays with 3 odd numbers are [1,1,2,1] and [1,2,1,1].
 // Example 2:
@@ -14,14 +13,19 @@ let ctr = 0,
 // Input: nums = [2,2,2,1,2,2,1,2,2,2], k = 2
 // Output: 16
 
-for (i = 0; i < nums.length; i++) {
-  for (j = 0; j < nums.length; j++) {
-    if (nums[i] % 2 === 1) {
-      res.push(nums[i]);
-    }
+let odd = [];
+
+for (let i = 0; i < nums.length; i++) {
+  if (nums[i] % 2 === 1) {
+    odd.push(nums[i]);
+    nums.splice(i, 1);
+    i--;
   }
-  ctr++;
 }
 
-console.log(res);
-console.log(ctr);
+for(let i =0;i<nums.length;i++){
+  
+}
+
+console.log(odd);
+console.log(nums);
