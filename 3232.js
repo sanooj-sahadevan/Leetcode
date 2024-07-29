39093d3e5f1eb4d6960e493105f9e6da1ef7f759
sanooj <146158357+sanooj-sahadevan@users.noreply.github.com>
2024-07-29 11:24:16 +0530

    add

---
+++ 3232.js
@@ -0,0 +1,32 @@
+let nums = [1, 2, 3, 4, 10];
+// let nums = [1];
+// Output: false
+// Explanation:
+// Alice cannot win by choosing either single-digit or double-digit numbers.
+// Example 2:
+// Input: nums = [1, 2, 3, 4, 5, 14];
+// Output: true
+// Explanation:
+// Alice can win by choosing single-digit numbers which have a sum equal to 15.
+// Example 3:
+// Input: nums = [5, 5, 5, 25];
+// Output: true
+
+let a = 0;
+let x = nums.filter((curr, ind) => curr < 10).reduce((acc, curr) => acc + curr,0);
+let y = nums.filter((curr, ind) => curr >= 10).reduce((acc, curr) => acc + curr,0);
+console.log(x);
+console.log(y);
+console.log(x == y ? false : true);
+
+// for (i = 0; i < nums.length; i++) {
+//   if (nums[i] <= 10) {
+//     a = nums[i];
+//   }
+// }
+
+// if (x <= a) {
+//   console.log(false);
+// } else {
+//   console.log(true);
+// }
