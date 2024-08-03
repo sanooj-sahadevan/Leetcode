16ec11012aeb7249887bbdefcb56a71fb3fb7ab5
sanooj <146158357+sanooj-sahadevan@users.noreply.github.com>
2024-08-03 09:50:22 +0530

    addd

--- 2535.js
+++ 2535.js
@@ -13,11 +13,12 @@ Input: nums = [1, 15, 6, 3];
 // The digit sum of nums is 1 + 2 + 3 + 4 = 10.
 // The absolute difference between the element sum and digit sum is |10 - 10| = 0.
 
-let digit = nums
-  .join("")
-  .split("")
-  .map((v) => Number(v))
-  .reduce((acc, curr) => acc + curr);
-let element = nums.reduce((acc, curr) => acc + curr);
+// let digit = nums
+//   .join("")
+//   .split("")
+//   .map((v) => Number(v))
+//   .reduce((acc, curr) => acc + curr);
+// let element = nums.reduce((acc, curr) => acc + curr);
 
-console.log(Math.abs(element - digit));
+// console.log(Math.abs(element - digit));
+
