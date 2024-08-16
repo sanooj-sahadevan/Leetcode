34ce07a3a00cf1f97898614184d0af9174ded4e8
sanooj <146158357+sanooj-sahadevan@users.noreply.github.com>
2024-08-16 10:06:37 +0530

    addd

--- workout.js
+++ workout.js
@@ -1,68 +1,49 @@
-// Input: (left = 5), (right = 8);
-// // Output: 3
-// // Explanation:
-// // 6  -> 110 (2 set bits, 2 is prime)
-// // 7  -> 111 (3 set bits, 3 is prime)
-// // 8  -> 1000 (1 set bit, 1 is not prime)
-// // 9  -> 1001 (2 set bits, 2 is prime)
-// // 10 -> 1010 (2 set bits, 2 is prime)
-// // 4 numbers have a prime number of set bits.
-// // Example 2:
+Input: arrays = [
+  [1, 4],
+  [0, 5],
+];
 
-// // Input: (left = 10), (right = 15);
-// // Output: 5
-// // Explanation:
-
-// // 10 -> 1010 (2 set bits, 2 is prime)
-// // 11 -> 1011 (3 set bits, 3 is prime)
-// // 12 -> 1100 (2 set bits, 2 is prime)
-// // 13 -> 1101 (3 set bits, 3 is prime)
-// // 14 -> 1110 (3 set bits, 3 is prime)
-// // 15 -> 1111 (4 set bits, 4 is not prime)
-// // 5 numbers have a prime number of set bits.
-// let a = 6;
-// let num = a.toString(2);
-// console.log(num);
-// let res = [];
-
-// for (i = left; i <= right; i++) {
-//   res.push(i.toString(2));
-// }
-// let res2 = [];
-// for (i = 0; i < res.length; i++) {
-//   let ctr = 0;
-//   for (j = 0; j < res[i].length; j++) {
-//     if (res[i][j] == 1) {
-//       ctr++;
-//     }
-//   }
-//   res2.push(ctr);
-// }
-
-// let flag = 0;
-// for (i = 0; i < res2.length; i++) {
-//   if (res2[i] == 1) {
-//     res2.splice(i, 1);
-//   }
-//   let ctrr = 1;
-//   for (j = 2; j < res2[i]; j++) {
-//     if (res2[i] % j == 0) {
-//       ctrr = 0;
-//     }
-//   }
-//   if (ctrr) {
-//     flag++;
-//   }
-// }
-
-// console.log(flag);
-// console.log(res2);
-// // 2, 3, 5, 7, 11, 13
-
-Input: (left = 1), (right = 22);
-// Output: [1,2,3,4,5,6,7,8,9,11,12,15,22]
+// Output: 4
+// Explanation: One way to reach the maximum distance 4 is to pick 1 in the first or third array and pick 5 in the second array.
 // Example 2:
 
-// Input: left = 47, right = 85
-// Output: [48,55,66,77]
-                               
+// Input: arrays = [[1],[1]]
+// Output: 0
+
+// let a = arrays.flat();
+
+// for(i=0;i<a.length;i++){
+//     if(a[i]==0){
+//         a.splice(i,1)
+//     }
+// }
+// let large = Math.max(...a);
+// let small = Math.min(...a);
+
+// console.log(a);
+// console.log(large-small);
+
+// console.log(large);
+// console.log(small);
+let res = [],
+  small = [];
+for (i = 0; i < arrays.length; i++) {
+  for (j = 0; j < arrays[i].length; j++) {
+    let a = Math.max(...arrays[i]);
+    res.push(a);
+    let b = Math.min(...arrays[i]);
+    small.push(b);
+  }
+}
+
+small = small.filter((curr, ind) => small.indexOf(curr) !== ind); //repred elements
+
+for (i = 0; i < small.length; i++) {
+  if (small[i] == 0) {
+    small.splice(i, 1);
+  }
+}
+console.log(Math.max(...res) - Math.min(...small));
+
+console.log(res);
+console.log(small);
