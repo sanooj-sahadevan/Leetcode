63f8a34ab89ec2f00af5084c88f259d4416c02af
sanooj <146158357+sanooj-sahadevan@users.noreply.github.com>
2024-08-17 12:33:01 +0530

    addd

--- workout.js
+++ workout.js
@@ -1,49 +1,21 @@
-Input: arrays = [
-  [1, 4],
-  [0, 5],
-];
+Input: (s = "abc"), (t = "bac");
+// Output: 2
+// Explanation:
+// For s = "abc" and t = "bac", the permutation difference of s and t is equal to the sum of:
+// The absolute difference between the index of the occurrence of "a" in s and the index of the occurrence of "a" in t.
+// The absolute difference between the index of the occurrence of "b" in s and the index of the occurrence of "b" in t.
+// The absolute difference between the index of the occurrence of "c" in s and the index of the occurrence of "c" in t.
+// That is, the permutation difference between s and t is equal to |0 - 1| + |2 - 2| + |1 - 0| = 2.
 
-// Output: 4
-// Explanation: One way to reach the maximum distance 4 is to pick 1 in the first or third array and pick 5 in the second array.
-// Example 2:
-
-// Input: arrays = [[1],[1]]
-// Output: 0
-
-// let a = arrays.flat();
-
-// for(i=0;i<a.length;i++){
-//     if(a[i]==0){
-//         a.splice(i,1)
-//     }
-// }
-// let large = Math.max(...a);
-// let small = Math.min(...a);
-
-// console.log(a);
-// console.log(large-small);
-
-// console.log(large);
-// console.log(small);
-let res = [],
-  small = [];
-for (i = 0; i < arrays.length; i++) {
-  for (j = 0; j < arrays[i].length; j++) {
-    let a = Math.max(...arrays[i]);
-    res.push(a);
-    let b = Math.min(...arrays[i]);
-    small.push(b);
+s = s.split("");
+t = t.split("");
+console.log(s);
+let res = [];
+for (i = 0; i < s.length; i++) {
+  if (t.includes(s[i])) {
+    res.push([Math.abs(t.indexOf(s[i]) - i)]);
   }
 }
 
-small = small.filter((curr, ind) => small.indexOf(curr) !== ind); //repred elements
-
-for (i = 0; i < small.length; i++) {
-  if (small[i] == 0) {
-    small.splice(i, 1);
-  }
-}
-console.log(Math.max(...res) - Math.min(...small));
 
 console.log(res);
-console.log(small);
