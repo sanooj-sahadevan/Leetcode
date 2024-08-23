bffae68ddb8e20ed75d5952cd25a932fccc6febe
sanooj <146158357+sanooj-sahadevan@users.noreply.github.com>
2024-08-23 10:00:44 +0530

    addd

--- workout.js
+++ workout.js
@@ -1,29 +1,17 @@
-let num = 9669;
-// Output: 9969
-// Explanation:
-// Changing the first digit results in 6669.
-// Changing the second digit results in 9969.
-// Changing the third digit results in 9699.
-// Changing the fourth digit results in 9666.
-// The maximum number is 9969.
+// Input: (n = 5), (start = 0);
+// Output: 8
+// Explanation: Array nums is equal to [0, 2, 4, 6, 8] where (0 ^ 2 ^ 4 ^ 6 ^ 8) = 8.
+// Where "^" corresponds to bitwise XOR operator.
+// Example 2:
 
-num = num.toString().split("");
+Input: n = 4, start = 3
+// Output: 8
+// Explanation: Array nums is equal to [3, 5, 7, 9] where (3 ^ 5 ^ 7 ^ 9) = 8.
+
+
 let res = [];
-
-for (let j = 0; j < num.length; j++) {
-  let original = num[j];
-
-
-
-  
-  if (num[j] == "9") {
-    num[j] = "6";
-  } else if (num[j] == "6") {
-    num[j] = "9";
-  }
-
-  res.push(num.join(""));
-  num[j] = original;
+for (let i = 0; i < n; i++) {
+  res.push(start + 2 * i);
 }
+return res.reduce((acc, num) => acc ^ num, 0);
 
-console.log(res);
