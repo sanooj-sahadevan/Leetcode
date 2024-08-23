92e70b7794db08f1c10db1c7bf79fa0e9e467dfa
Sanooj Sahadevan <146158357+sanooj-sahadevan@users.noreply.github.com>
2024-08-23 09:42:16 +0530

    Time: 40 ms (94.74%) | Memory: 49.7 MB (8.19%) - LeetSync

--- 1289-day-of-the-week/day-of-the-week.js
+++ 1289-day-of-the-week/day-of-the-week.js
@@ -8,6 +8,6 @@ var dayOfTheWeek = function (day, month, year) {
 
     let obj = { Sun: 'Sunday', Mon: 'Monday', Tue: "Tuesday", Wed: 'Wednesday', Thu: 'Thursday', Fri: "Friday", Sat: "Saturday" }
     let date = new Date(`${year}-${month}-${day}`);
-   let dayAbbreviation = date.toDateString().substring(0, 3);
-    return obj[dayAbbreviation];
+    let x = date.toDateString().substring(0, 3);
+    return obj[x];
 };
