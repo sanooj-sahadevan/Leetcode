c031a2604ea5c5c531044933892374124fb4539e
Sanooj Sahadevan <146158357+sanooj-sahadevan@users.noreply.github.com>
2024-08-25 22:29:54 +0530

    Time: 60 ms (22.42%) | Memory: 49.4 MB (46.30%) - LeetSync

--- 392-is-subsequence/is-subsequence.js
+++ 392-is-subsequence/is-subsequence.js
@@ -3,16 +3,16 @@
  * @param {string} t
  * @return {boolean}
  */
-var isSubsequence = function(s, t) {
+var isSubsequence = function (s, t) {
 
-  if (s.length > t.length) return false;
-  const t_length = t.length;
-  let subsequence = 0;
-  for (let i = 0; i < t_length; i++) {
-    if (s[subsequence] === t[i]) {
-      subsequence++;
+    if (s.length > t.length) return false;
+    const t_length = t.length;
+    let subsequence = 0;
+    for (let i = 0; i < t_length; i++) {
+        if (s[subsequence] === t[i]) {
+            subsequence++;
+        }
     }
-  }
-  return subsequence === s.length
+    return subsequence === s.length
 };
-    
+
