0cdf4c8ec0ea55f5a74e086f7aea1bc41c1e2ac9
sanooj <146158357+sanooj-sahadevan@users.noreply.github.com>
2024-08-28 10:51:06 +0530

    addd

--- workout.js
+++ workout.js
@@ -1,13 +1,45 @@
-let nums = [1, 2, 3];
-// Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+// let bills = [5, 5, 5, 10, 20];
+// Output: true
+// Explanation:
+// From the first 3 customers, we collect three $5 bills in order.
+// From the fourth customer, we collect a $10 bill and give back a $5.
+// From the fifth customer, we give a $10 bill and a $5 bill.
+// Since all customers got correct change, we output true.
 
-let res = [[]];
+Input: bills = [5, 5, 10, 10, 20];
+// Output: false 15
+// Explanation:
+// From the first two customers in order, we collect two $5 bills.
+// For the next two customers in order, we collect a $10 bill and give back a $5 bill.
+// For the last customer, we can not give the change of $15 back because we only have two $10 bills.
+// Since not every customer received the correct change, the answer is false.
 
-for (let i = 0; i < nums.length; i++) {
-  let currentSubsetSize = res.length;
-  for (let j = 0; j < currentSubsetSize; j++) {
-    res.push([...res[j], nums[i]]);
+
+let fiveCount = 0;
+let tenCount = 0;
+
+for (let i = 0; i < bills.length; i++) {
+  if (bills[i] === 5) {
+    fiveCount++;
+  } else if (bills[i] === 10) {
+    if (fiveCount > 0) {
+      fiveCount--;
+      tenCount++;
+    } else {
+      console.log(false);
+      return;
+    }
+  } else if (bills[i] === 20) {
+    if (tenCount > 0 && fiveCount > 0) {
+      tenCount--;
+      fiveCount--;
+    } else if (fiveCount >= 3) {
+      fiveCount -= 3;
+    } else {
+      console.log(false);
+      return;
+    }
   }
 }
 
-console.log(res);
+console.log(true);
