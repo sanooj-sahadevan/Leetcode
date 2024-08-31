a7423af5444ba7d1a465a57f7de7f537e7ff6338
sanooj <146158357+sanooj-sahadevan@users.noreply.github.com>
2024-08-31 10:07:34 +0530

    addd

--- workout.js
+++ workout.js
@@ -1,13 +1,25 @@
-let prices = [7, 1, 5, 3, 6, 4];
-// Output: 5
-// Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
-// Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+let nums = [1, 3, 5, 4, 7];
+// Output: 3
+// Explanation: The longest continuous increasing subsequence is [1,3,5] with length 3.
+// Even though [1,3,5,7] is an increasing subsequence, it is not continuous as elements 5 and 7 are separated by element
+// 4.
 // Example 2:
 
-// Input: prices = [1, 4, 2];
+// Input: nums = [2,2,2,2,2]
+// Output: 1
+// Explanation: The longest continuous increasing subsequence is [2] with length 1. Note that it must be strictly
+// increasing.
 
-// Output: 0
-// Explanation: In this case, no transactions are done and the max profit = 0.
+let length = 1;
+let ctr = 1;
 
+for (let i = 1; i < nums.length; i++) {
+  if (nums[i] > nums[i - 1]) {
+    ctr++;
+    length = Math.max(length, ctr);
+  } else {
+    ctr = 1;
+  }
+}
 
-
+console.log(length);
