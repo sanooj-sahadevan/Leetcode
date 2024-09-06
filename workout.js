173cf41105e68d237da9b291b7784c2c193f60ab
sanooj <146158357+sanooj-sahadevan@users.noreply.github.com>
2024-09-06 14:08:57 +0530

    add

--- workout.js
+++ workout.js
@@ -1,51 +1,27 @@
-let coordinate1 = "a7",
-  coordinate2 = "a6";
+Input: moves = "UD";
 // Output: true
-// Eplanation:
-// Boh squares are black.
+// Explanation: The robot moves up once, and then down once. All moves have the same magnitude, so it ended up at the origin where it started. Therefore, we return true.
 // Example 2:
-// Input: coordinate1 = "a1", coordinate2 = "h3"
+
+// Input: moves = "LL"
 // Output: false
-// Explanation:
-// Square "a1" is black and "h3" is white.
+// Explanation: The robot moves left twice. It ends up two "moves" to the left of the origin. We return false because it is not at the origin at the end of its moves.
 
-let obj = {
-  a: 1,
-  b: 2,
-  c: 3,
-  d: 4,
-  e: 5,
-  f: 6,
-  g: 7,
-  h: 8,
-  i: 9,
-  j: 10,
-  k: 11,
-  l: 12,
-  m: 14,
-  n: 14,
-  o: 15,
-  p: 16,
-  q: 17,
-  r: 18,
-  s: 19,
-  t: 20,
-  u: 21,
-  v: 22,
-  w: 23,
-  x: 24,
-  y: 25,
-  z: 16,
-};
-let a = coordinate1.split("");
-let b = coordinate2.split("");
+moves.split("");
+let sum = 0;
+for (i = 0; i < moves.length; i++) {
+  if (moves[i] === "U") {
+    sum += 1;
+  }
+  if (moves[i] === "D") {
+    sum -= 1;
+  }
+  if (moves[i] === "L") {
+    sum += 1;
+  }
+  if (moves[i] === "R") {
+    sum -= 1;
+  }
+}
 
-let sum1 = obj[a[0]] + parseInt(a[1]);
-let sum2 = obj[b[0]] + parseInt(b[1]);
-
-let result = (sum1 % 2 === sum2 % 2);
-
-console.log(result);
-
-// console.log(x);
-// console.log(y);
+if(sum==0)
