c8ed3d058b91cbdc25578c5015b71616a0b9c6f9
Sanooj Sahadevan <146158357+sanooj-sahadevan@users.noreply.github.com>
2024-09-10 09:24:11 +0530

    Time: 61 ms (5.31%) | Memory: 49 MB (20.71%) - LeetSync

---
+++ 500-keyboard-row/keyboard-row.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string[]} words
+ * @return {string[]}
+ */
+var findWords = function (words) {
+   
+
+let a = "qwertyuiop".split("");
+let b = "asdfghjkl".split("");
+let c = "zxcvbnm".split("");
+console.log(a);
+
+
+let res = [];
+
+for (let i = 0; i < words.length; i++) {
+  let lowerWord = words[i].toLowerCase(); // Make it case-insensitive
+  if (
+    lowerWord.split("").every(char => a.includes(char)) || 
+    lowerWord.split("").every(char => b.includes(char)) || 
+    lowerWord.split("").every(char => c.includes(char))
+  ) {
+    res.push(words[i]);
+  }
+}
+
+console.log(res);
+return res;
+
+
+
+};
