2526afba65589c26a1dd165aca4b4a788457e277
sanooj <146158357+sanooj-sahadevan@users.noreply.github.com>
2024-09-19 09:39:41 +0530

    add

--- workout.js
+++ workout.js
@@ -1,64 +1,27 @@
-Input: (nums1 = [1, 2, 3, 0, 0, 0]), (m = 3), (nums2 = [2, 5, 6]), (n = 3);
-// Output: [1,2,2,3,5,6]
-// Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
-// The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+let ranges = [[1,2],[3,4],[5,6]], left = 2, right = 5
+// Output: true
+// Explanation: Every integer between 2 and 5 is covered:
+// - 2 is covered by the first range.
+// - 3 and 4 are covered by the second range.
+// - 5 is covered by the third range.
 // Example 2:
 
-// Input: nums1 = [1], m = 1, nums2 = [], n = 0
-// Output: [1]
-// Explanation: The arrays we are merging are [1] and [].
-// The result of the merge is [1].
-// Example 3:
+// Input: ranges = [[1,10],[10,20]], left = 21, right = 21
+// Output: false
+// Explanation: 21 is not covered by any range.
 
-// Input: nums1 = [0], m = 0, nums2 = [1], n = 1
-// Output: [1]
-// Explanation: The arrays we are merging are [] and [1].
-// The result of the merge is [1].
-// Note that because m = 0, there are no elements in nums1. The 0 is only there to ensure the merge result can fit in nums1.
 
-let x = [...nums1, ...nums2];
+ let x = ranges.flat(Infinity)
+ console.log(x);
 
-for (i = 0; i < x.length; i++) {
-  if (x[i] <= 0) {
-    x[i] = x[1 + 1];
-    i--;
-  }
-}
-
-for (let i = 0; i < x.length; i++) {
-  for (j = 0; j < x.length - 1; j++) {
-    if (x[j] > x[j + 1]) {
-      let temp = x[j];
-      x[j] = x[j + 1];
-      x[j + 1] = temp;
+ for(i =0;i<ranges.length;i++){
+  for(j=i+1;j<ranges.length;j++){
+    if(  ){
+    console.log(true);
+    
     }
   }
-}
-
-/**
- * @param {number[]} nums1
- * @param {number} m
- * @param {number[]} nums2
- * @param {number} n
- * @return {void} Do not return anything, modify nums1 in-place instead.
- */
-var merge = function (nums1, m, nums2, n) {
-  var i = m - 1;
-  var j = n - 1;
-  var k = m + n - 1;
-  while (i >= 0 && j >= 0) {
-    if (nums1[i] > nums2[j]) {
-      nums1[k] = nums1[i];
-      i--;
-    } else {
-      nums1[k] = nums2[j];
-      j--;
-    }
-    k--;
-  }
-  while (j >= 0) {
-    nums1[k] = nums2[j];
-    j--;
-    k--;
-  }
-};
+ console.log(false);
+ 
+ }
+ 
