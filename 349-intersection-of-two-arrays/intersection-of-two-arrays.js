d2f0a62bb4d3f1424ffe398f5afc28d225aed594
Sanooj Sahadevan <146158357+sanooj-sahadevan@users.noreply.github.com>
2024-10-01 09:17:19 +0530

    Time: 55 ms (61.07%) | Memory: 51.2 MB (22.45%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.js
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.js
@@ -6,9 +6,7 @@
 var intersection = function(nums1, nums2) {
     
     var nums1= nums1.filter( (val,index)=>nums1.indexOf(val)==index )
-
     var nums2= nums2.filter( (val,index)=>nums2.indexOf(val)==index )
-
     return nums1.filter( val=>nums2.includes(val)   )
 
 };
