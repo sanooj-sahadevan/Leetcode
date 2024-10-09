254b7af36147c947d8db98e97cde20b87e4ff40e
Sanooj Sahadevan <146158357+sanooj-sahadevan@users.noreply.github.com>
2024-10-09 10:01:35 +0530

    Time: 75 ms (20.39%) | Memory: 51.7 MB (78.02%) - LeetSync

---
+++ 941-sort-array-by-parity/sort-array-by-parity.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortArrayByParity = function (nums) {
+    let x = []
+    let y = []
+    nums.sort()
+    for (i = 0; i < nums.length; i++) {
+        if (nums[i] % 2 == 0) {
+            x.push(nums[i])
+        } else {
+            y.push(nums[i])
+        }
+    }
+    return [...x, ...y]
+};
