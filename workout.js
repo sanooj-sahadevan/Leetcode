e3547336cde034a74adfe13ba0dc80919edf2bd5
sanooj <146158357+sanooj-sahadevan@users.noreply.github.com>
2024-10-18 09:38:31 +0530

    add

--- workout.js
+++ workout.js
@@ -1,27 +1,40 @@
-Input: rowIndex = 3;
-// Output: [1,3,3,1]
+// let s = "leetcode";
+// Output: 0
+// xplanation:
+// The character 'l' at index 0 is the first character that does not occur at any other index.
 // Example 2:
-
-// Input: rowIndex = 0
-// Output: [1]
+Input: s = "loveleetcode";
+// Output: 2
 // Example 3:
-
-// Input: rowIndex = 1
-// Output: [1,1]
-const triangle = [];
-
-for (let n = 0; n < rowIndex + 1; n++) {
-  const row = [1];
-
-  for (let k = 1; k < n; k++) {
-    row[k] = triangle[n - 1][k - 1] + triangle[n - 1][k];
-  }
-
-  if (n > 0) row.push(1);
-  triangle.push(row);
+// Input: s = "aabb"
+// Output: -1
+let res = 0
+let f = {},
+  x = [];
+let y = s.split("");
+for (let i = 0; i < y.length; i++) {
+  f[y[i]] = 0;
 }
 
-console.log(triangle[triangle.length - 1]);
+for (let i = 0; i < y.length; i++) {
+  f[y[i]]++;
+}
 
-console.log(triangle);
+for(let key in f){
+  if(f[key]==1){
+    res = key
+    break;
+  }
+}
 
+
+for(i =0;i<y.length;i++){
+  if(y[i]===res){
+    console.log(i);
+    break
+  }
+}
+
+
+console.log(res);
+console.log(f);
