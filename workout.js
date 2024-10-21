c3965c096b59abfe6bd5ab11c22010bdc02ce72b
sanooj <146158357+sanooj-sahadevan@users.noreply.github.com>
2024-10-21 10:50:49 +0530

    add

--- workout.js
+++ workout.js
@@ -1,33 +1,15 @@
-// Input: nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
-// Output: 6
-// Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+Input: nums = [10, 12, 13, 14];
 
-// let nums = [1]
-// Output: 1
-// Explanation: The subarray [1] has the largest sum 1.
-// Example 3:
+Output: 1;
 
-// Input: nums = [5, 4, -1, 7, 8];
-// Output: 23
-// Explanation: The subarray [5,4,-1,7,8] has the largest sum 23.
+// Explanation:
 
-let max = -Infinity;
-let sum = 0;
-const results = [];
-for (const num of nums) {
-  results.push(num);
-  sum += num;
-  max = Math.max(sum, max);
-  if (sum < 0) {
-    sum = 0;
-    results.length = 0;
-  }
+// nums becomes [1, 3, 4, 5] after all replacements, with minimum element 1.
+let arr = [];
+for (let i = 0; i < nums.length; i++) {
+  let res = nums[i].toString().split("").map(Number);
+  let sum = res.reduce((acc, curr) => acc + curr, 0);
+  console.log(sum);
+  arr.push(sum);
 }
-let res = 0;
-for (i = 0; i < results.length; i++) {
-  res += results[i];
-}
-
-console.log(results);
-
-console.log(res);
+return Math.min(...arr);
