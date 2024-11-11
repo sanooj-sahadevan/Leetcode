494f140700e7a10b4c507b9f62d7f9cd0be3f9de
Sanooj Sahadevan <146158357+sanooj-sahadevan@users.noreply.github.com>
2024-11-11 10:45:39 +0530

    Time: 17 ms (6.42%) | Memory: 53.8 MB (5.21%) - LeetSync

--- 3636-check-balanced-string/check-balanced-string.js
+++ 3636-check-balanced-string/check-balanced-string.js
@@ -2,30 +2,27 @@
  * @param {string} num
  * @return {boolean}
  */
-var isBalanced = function(num) {
+var isBalanced = function (num) {
     let even = [],
-  odd = [];
-for (let i = 0; i < num.length; i++) {
-  if (i % 2 == 0) {
-    even.push(Number(num[i]));
-  } else {
-    odd.push(Number(num[i]));
-  }
-}
+        odd = [];
+    for (let i = 0; i < num.length; i++) {
+        if (i % 2 == 0) {
+            even.push(Number(num[i]));
+        } else {
+            odd.push(Number(num[i]));
+        }
+    }
 
-let sum1 = even.reduce((a,b)=>a+b)
-let sum2 = odd.reduce((a,b)=>a+b)
+    let sum1 = even.reduce((a, b) => a + b)
+    let sum2 = odd.reduce((a, b) => a + b)
 
-console.log(sum1);
-console.log(sum2);
-if(sum1===sum2){
-    return true
-  console.log('true');
-  
-}else{
+    console.log(sum1);
+    console.log(sum2);
+    if (sum1 === sum2) {
+        return true
+        console.log('true');
+
+    } 
     return false
-  console.log('false');
-  
-}
 
 }
