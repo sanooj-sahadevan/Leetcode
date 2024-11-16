4f6920fa9c599e8bec4e3f7e271fc39ca3dac63d
sanooj <146158357+sanooj-sahadevan@users.noreply.github.com>
2024-11-16 10:02:38 +0530

    adddd

--- workout.js
+++ workout.js
@@ -1,17 +1,19 @@
+let date = "2080-02-29";
+// Output: "100000100000-10-11101"
+// Explanation:
+// 100000100000, 10, and 11101 are the binary representations of 2080, 02, and 29 respectively.
+// Example 2:
+// Input: date = "1900-01-01"
+// Output: "11101101100-1-1"
 
-let str = ["eat", "tea", "tan", "ate", "nat", "bat"];
-Output: [["bat"], ["nat", "tan"], ["ate", "eat", "tea"]];
+let x = date.split("-");
 
+let res = [];
 
-let b = [];
-
-for (i = 0; i < str.length; i++) {
-  let res = str[i].split("").sort().join("");
-  b.push(res);
+for (let i = 0; i < x.length; i++) {
+  let binary = Number(x[i]).toString(2);
+  console.log(binary);
+  res.push(binary);
 }
 
-
-
-
-
-// [ 'aet', 'aet', 'ant', 'aet', 'ant', 'abt' ]
+console.log(res.join('-'));
