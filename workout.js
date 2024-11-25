75b74b77d498e770e69213700ae774b38343f9d2
sanooj <146158357+sanooj-sahadevan@users.noreply.github.com>
2024-11-25 09:39:32 +0530

    addd

--- workout.js
+++ workout.js
@@ -1,62 +1,19 @@
-let s = "ccc";
-// Output: 3
-// Explanation: One longest palindrome that can be built is "dccaccd", whose length is 7.
+// let gain = [-5,1,5,0,-7]
+// Output: 1
+// Explanation: The altitudes are [0,-5,-4,1,1,-6]. The highest is 1.
 // Example 2:
 
-// Input: s = "a"
-// Output: 1
-// Explanation: The longest palindrome that can be built is "a", whose length is 1.
+Input: gain = [-4,-3,-2,-1,4,3,2]
+// Output: 0
+// Explanation: The altitudes are [0,-4,-7,-9,-10,-6,-3,-1]. The highest is 0.
 
-s = s.split("");
 
-let f = {},
-  x = [],
-  y = [];
-
-for (let i = 0; i < s.length; i++) {
-  f[s[i]] = 0;
+let sum = 0
+let x = [0]
+for(let i = 0;i<gain.length;i++){
+sum +=gain[i]
+x.push(sum)
 }
+console.log(x);
 
-for (let i = 0; i < s.length; i++) {
-  f[s[i]]++;
-}
-
-for (let key in f) {
-  if (f[key] % 2 === 0) {
-    x.push(f[key]);
-  }
-}
-
-for (let key in f) {
-  if (f[key] % 2 === 1) {
-    y.push(f[key]);
-  }
-}
-let sum = 0;
-for (let i = 0; i < y.length; i++) {
-  if (y[i] > 1) {
-    y[i] = y[i] - 1;
-    sum += y[i];
-  }
-}
-
-for (let i = 0; i < y.length; i++) {
-  if (y[i] === 1) {
-    x.push(y[i]);
-    break;
-  }
-}
-
-let result = 0
-
-for(let i =0;i<x.length;i++){
-    sum+=x[i]
-}
-
-let res = [result, sum];
-let ww = res.reduce((a, b) => a + b);
-
-console.log(ww);
-
-console.log(result + sum, "result");
-console.log(ww);
+console.log(Math.max(...x));
