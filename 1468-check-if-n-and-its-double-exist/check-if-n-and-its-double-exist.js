2c94f0f146990000eb3f88523ce44a54b91398b8
Sanooj Sahadevan <146158357+sanooj-sahadevan@users.noreply.github.com>
2024-11-25 09:14:09 +0530

    Time: 1 ms (84.78%) | Memory: 48.6 MB (97.11%) - LeetSync

--- 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.js
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.js
@@ -2,10 +2,10 @@
  * @param {number[]} arr
  * @return {boolean}
  */
-var checkIfExist = function(arr) {
-    for(i =0;i<arr.length;i++){
-        for(j=0;j<arr.length;j++){
-            if(arr[i] == 2 * arr[j] &&i != j  ){
+var checkIfExist = function (arr) {
+    for (i = 0; i < arr.length; i++) {
+        for (j = 0; j < arr.length; j++) {
+            if (arr[i] == 2 * arr[j] && i != j) {
                 return true
             }
         }
