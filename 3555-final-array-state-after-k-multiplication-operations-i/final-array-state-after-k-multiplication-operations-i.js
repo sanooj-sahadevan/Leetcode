8fec588cb5bf9d8f67efbfa3dae9a0e862b726da
Sanooj Sahadevan <146158357+sanooj-sahadevan@users.noreply.github.com>
2024-11-26 10:19:27 +0530

    Time: 3 ms (28.43%) | Memory: 52.4 MB (33.65%) - LeetSync

--- 3555-final-array-state-after-k-multiplication-operations-i/final-array-state-after-k-multiplication-operations-i.js
+++ 3555-final-array-state-after-k-multiplication-operations-i/final-array-state-after-k-multiplication-operations-i.js
@@ -9,9 +9,7 @@ var getFinalState = function (nums, k, multiplier) {
     for (let i = 0; i < k; i++) {
         let res = Math.min(...nums);
         let index = nums.indexOf(res)
-        res = res * multiplier;
-        console.log(res);
-        nums[index] = res
+        nums[index] = res * multiplier;
     }
     return nums
 };
