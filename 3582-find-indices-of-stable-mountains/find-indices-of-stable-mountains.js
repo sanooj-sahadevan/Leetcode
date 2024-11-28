0f965efdde87aed27e3b1772334bde07c969fa4d
Sanooj Sahadevan <146158357+sanooj-sahadevan@users.noreply.github.com>
2024-11-28 10:42:58 +0530

    Time: 1 ms (74.04%) | Memory: 52.4 MB (39.35%) - LeetSync

---
+++ 3582-find-indices-of-stable-mountains/find-indices-of-stable-mountains.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} height
+ * @param {number} threshold
+ * @return {number[]}
+ */
+var stableMountains = function (height, threshold) {
+    const result = []
+    for (let i = 1; i < height.length; i++) {
+        if (height[i - 1] > threshold)
+            result.push(i)
+    }
+    return result;
+
+};
