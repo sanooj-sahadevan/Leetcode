9f4a1e39fd888b5c693c5299e7251547f656960a
sanooj <146158357+sanooj-sahadevan@users.noreply.github.com>
2024-11-29 10:09:22 +0530

    initial commit new Branch

--- workout.js
+++ workout.js
@@ -1,33 +1,37 @@
-let groupSizes = [3, 3, 3, 3, 3, 1, 3];
-// Output: [[5],[0,1,2],[3,4,6]]
-// Explanation:
-// The first group is [5]. The size is 1, and groupSizes[5] = 1.
-// The second group is [0,1,2]. The size is 3, and groupSizes[0] = groupSizes[1] = groupSizes[2] = 3.
-// The third group is [3,4,6]. The size is 3, and groupSizes[3] = groupSizes[4] = groupSizes[6] = 3.
-// Other possible solutions are [[2,1,6],[5],[0,4,3]] and [[5],[0,6,2],[4,3,1]].
+let s = "aba";
+// Output: 3
+// Explanation: Three palindromic strings: "a", "b", "c".
 // Example 2:
 
-// Input: groupSizes = [2,1,3,3,3,2]
-// Output: [[1],[0,5],[2,3,4]]
+// let s = "aaa";
+// Output: 6
+// Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
+
+s = s.split("");
 
 let f = {},
-  result = [];
+  x = [];
 
-for (let i = 0; i < groupSizes.length; i++) {
-  f[groupSizes[i]] = 0;
+for (let i = 0; i < s.length; i++) {
+  f[s[i]] = 0;
 }
 
-for (let i = 0; i < groupSizes.length; i++) {
-  f[groupSizes[i]]++;
+for (let i = 0; i < s.length; i++) {
+  f[s[i]]++;
 }
+console.log(f);
 
-for (let i = 0; i < groupSizes.length; i++) {
-  let size = groupSizes[i];
-  f[size].push(i);
-  if (f[size].length === size) {
-    result.push(f[size]);
-    f[size] = [];
+let ctr = 0;
+for (let key in f) {
+  if (f[key] === 1) {
+    ctr++;
+  }
+  if (f[key] % 2 !== 0 && f[key] !==1 ) {
+    ctr = f[key] * 2;
+
+  } else {
+    ctr = f[key] * 2 - 1;
+
   }
 }
-
-console.log(result);
+console.log(ctr);
