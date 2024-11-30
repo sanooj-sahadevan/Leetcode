b822a0456c3c502c7e1ad3d57c21504fe69f37c6
Sanooj Sahadevan <146158357+sanooj-sahadevan@users.noreply.github.com>
2024-11-30 09:44:03 +0530

    Time: 12 ms (67.32%) | Memory: 57.6 MB (72.28%) - LeetSync

---
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var removeDuplicates = function(s) {
+    let stack = [];
+    for (let char of s) {
+        if (stack.length > 0 && stack[stack.length - 1] === char) {
+            stack.pop();
+        } else {
+            stack.push(char);
+        }
+    }
+    return stack.join(''); 
+};
+
