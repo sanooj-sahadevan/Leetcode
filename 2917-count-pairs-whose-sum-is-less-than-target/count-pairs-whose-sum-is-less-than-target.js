4b15ac5f72eb1049683e9635eab7ec468ea4886d
Sanooj Sahadevan <146158357+sanooj-sahadevan@users.noreply.github.com>
2024-12-02 15:10:16 +0530

    Time: 1 ms (81.70%) | Memory: 51.3 MB (65.84%) - LeetSync

---
+++ 2917-count-pairs-whose-sum-is-less-than-target/count-pairs-whose-sum-is-less-than-target.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var countPairs = function (nums, target) {
+
+
+    let count = 0
+
+    for (i = 0; i < nums.length; i++) {
+        for (j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] < target) {
+                count++
+            }
+        }
+    }
+    return count
+
+};
