5c5c2eb0af2b98c1ead4be6ae2f31e0235a06207
sanooj <146158357+sanooj-sahadevan@users.noreply.github.com>
2024-12-06 10:53:21 +0530

    add

--- workout.js
+++ workout.js
@@ -1,9 +1,25 @@
-let num1 = "2", num2 = "3"
-// Output: "6"
+// let s = "ab-cd"
+// Output: "dc-ba"
 // Example 2:
 
-// Input: num1 = "123", num2 = "456"
-// Output: "56088"
+// let s = "a-bC-dEf-ghIj";
+// Output: "j-Ih-gfE-dCba"
+// Example 3:
 
-let sum = Number(num1) * Number(num2)
-console.log(  BigInt(sum.toString()));
+Input: s = "Test1ng-Leet=code-Q!";
+// Output: "Qedo1ct-eeLg=ntse-T!
+
+s = s.split("");
+let temp;
+
+for (let i = 0; i < Math.floor(s.length / 2); i++) {
+  if (/[a-zA-Z]/) {
+    temp = s[i];
+    s[i] = s[s.length - 1 - i];
+    s[s.length - 1 - i] = temp;
+  } else {
+    continue;
+  }
+}
+console.log(s.join(""));
+// return s.join('')
