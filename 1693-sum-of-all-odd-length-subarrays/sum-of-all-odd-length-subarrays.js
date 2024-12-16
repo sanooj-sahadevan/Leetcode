e258216b4e006e5d7eefd1cd5a5d4ee9930788a9
Sanooj Sahadevan <146158357+sanooj-sahadevan@users.noreply.github.com>
2024-12-16 10:36:09 +0530

    Time: 266 ms (5.22%) | Memory: 63.1 MB (5.74%) - LeetSync

---
+++ 1693-sum-of-all-odd-length-subarrays/sum-of-all-odd-length-subarrays.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var sumOddLengthSubarrays = function (arr) {
+
+    let x = [];
+    for (let i = 0; i < arr.length; i++) {
+        let z = i;
+        for (let j = 1; z + j <= arr.length; j += 2) {
+            x.push(arr.slice(z, z + j));
+        }
+    }
+    console.log(x);
+    x = x.flat(Infinity)
+    let sum = x.reduce((acc, curr) => acc + curr)
+    return sum
+    console.log(sum);
+
+};
