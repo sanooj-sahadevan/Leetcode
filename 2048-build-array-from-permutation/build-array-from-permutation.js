4eb5fcc38c90023c4554a5385c1ad8eb11c73da5
Sanooj Sahadevan <146158357+sanooj-sahadevan@users.noreply.github.com>
2024-12-20 09:15:57 +0530

    Time: 1 ms (74.77%) | Memory: 52.9 MB (69.31%) - LeetSync

--- 2048-build-array-from-permutation/build-array-from-permutation.js
+++ 2048-build-array-from-permutation/build-array-from-permutation.js
@@ -2,11 +2,10 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var buildArray = function(nums) {
+var buildArray = function (nums) {
     let res = []
-    for(i=0;i<nums.length;i++){
-        res.push(nums[nums[i]])
-    console.log(nums[nums[i]])
-}
+    for (i = 0; i < nums.length; i++) {
+        res[i] = nums[nums[i]]
+    }
     return res
 };
