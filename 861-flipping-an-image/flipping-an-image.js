7948577ce21d69a7d2df570fbc5a482e4edbe818
Sanooj Sahadevan <146158357+sanooj-sahadevan@users.noreply.github.com>
2024-12-23 10:47:25 +0530

    Time: 2 ms (34.62%) | Memory: 52 MB (64.56%) - LeetSync

--- 861-flipping-an-image/flipping-an-image.js
+++ 861-flipping-an-image/flipping-an-image.js
@@ -4,12 +4,10 @@
  */
 var flipAndInvertImage = function (image) {
     let x = 0;
-    for (let i = 0; i < image.length; i++) {
-        image[i].reverse();
-    }
-    console.log(image);
+
 
     for (let i = 0; i < image.length; i++) {
+        image[i].reverse();
         for (let j = 0; j < image[i].length; j++) {
             image[i][j] = image[i][j] === 0 ? 1 : 0;
         }
