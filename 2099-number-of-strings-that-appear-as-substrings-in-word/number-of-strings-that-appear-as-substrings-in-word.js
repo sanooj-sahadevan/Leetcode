8933baf7d90cbdc4707c90f89dd224598cee4abd
Sanooj Sahadevan <146158357+sanooj-sahadevan@users.noreply.github.com>
2024-12-24 09:57:04 +0530

    Time: 1 ms (21.54%) | Memory: 49.4 MB (29.67%) - LeetSync

---
+++ 2099-number-of-strings-that-appear-as-substrings-in-word/number-of-strings-that-appear-as-substrings-in-word.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string[]} patterns
+ * @param {string} word
+ * @return {number}
+ */
+var numOfStrings = function(patterns, word) {
+    let ctr=0
+for(let i =0;i<patterns.length;i++){
+  if(word.includes(patterns[i])){
+    ctr++
+  }
+}
+return ctr
+console.log(ctr);
+    
+};
