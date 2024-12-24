216374faf3bf8be1c1dfc91cf575ed19f7336a69
Sanooj Sahadevan <146158357+sanooj-sahadevan@users.noreply.github.com>
2024-12-24 09:57:12 +0530

    Time: 0 ms (100.00%) | Memory: 49.3 MB (44.31%) - LeetSync

--- 2099-number-of-strings-that-appear-as-substrings-in-word/number-of-strings-that-appear-as-substrings-in-word.js
+++ 2099-number-of-strings-that-appear-as-substrings-in-word/number-of-strings-that-appear-as-substrings-in-word.js
@@ -3,14 +3,13 @@
  * @param {string} word
  * @return {number}
  */
-var numOfStrings = function(patterns, word) {
-    let ctr=0
-for(let i =0;i<patterns.length;i++){
-  if(word.includes(patterns[i])){
-    ctr++
-  }
-}
-return ctr
-console.log(ctr);
-    
+var numOfStrings = function (patterns, word) {
+    let ctr = 0
+    for (let i = 0; i < patterns.length; i++) {
+        if (word.includes(patterns[i])) {
+            ctr++
+        }
+    }
+    return ctr
+
 };
