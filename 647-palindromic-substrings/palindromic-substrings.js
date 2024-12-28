b031425569d6e55942a5f6be4a219a3f1d6556d2
Sanooj Sahadevan <146158357+sanooj-sahadevan@users.noreply.github.com>
2024-12-28 09:38:36 +0530

    Time: 12 ms (78.71%) | Memory: 50.7 MB (52.22%) - LeetSync

---
+++ 647-palindromic-substrings/palindromic-substrings.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var countSubstrings = function (s) {
+    let count = 0;
+
+    const expandAroundCenter = (left, right) => {
+        while (left >= 0 && right < s.length && s[left] === s[right]) {
+            count++; // Found a palindrome
+            left--;  // Expand to the left
+            right++; // Expand to the right
+        }
+    };
+
+    for (let i = 0; i < s.length; i++) {
+        expandAroundCenter(i, i);
+        expandAroundCenter(i, i + 1);
+    }
+
+    return count;
+};
