da56c811b2a791eebd26e2719986c469bab71d1e
sanooj <146158357+sanooj-sahadevan@users.noreply.github.com>
2024-12-28 09:43:34 +0530

    add

--- workout.js
+++ workout.js
@@ -1,24 +1,19 @@
-//var nums = [3, 4, 5, 2];
-// Output: 12
-// Explanation: If you choose the indices i=1 and j=2 (indexed from 0), you will get the maximum value, that is, (nums[1]-1)*(nums[2]-1) = (4-1)*(5-1) = 3*4 = 12.
+//let s = "abc";
+// Output: 3
+// Explanation: Three palindromic strings: "a", "b", "c".
 // Example 2:
 
- Input: nums = [1,5,4,5]
-// Output: 16
-// Explanation: Choosing the indices i=1 and j=3 (indexed from 0), you will get the maximum value of (5-1)*(5-1) = 16.
+let s = "aba"
+// Output: 6
+// Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
 
-let largest = 0,
-  second = 0;
-for (let i = 0; i < nums.length; i++) {
-  if (largest < nums[i]) {
-    second = largest;
-    largest = nums[i];
-  } else if (largest >= nums[i] && second <= nums[i]) {
-    second = nums[i];
-  }
+s = s.split("");
+let f = {};
+for (let i = 0; i < s.length; i++) {
+  f[s[i]] = 0;
+}
+for (let i = 0; i < s.length; i++) {
+  f[s[i]]++;
 }
 
-console.log((largest - 1) * (second - 1));
-
-console.log(largest);
-console.log(second);
+console.log(f);
