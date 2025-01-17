9905805a369be2469c639cb8895cf9ecfc273d5d
Sanooj Sahadevan <146158357+sanooj-sahadevan@users.noreply.github.com>
2025-01-17 11:19:50 +0530

    Time: 191 ms (22.58%) | Memory: 64.4 MB (63.55%) - LeetSync

---
+++ 2760-snail-traversal/snail-traversal.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number} rowsCount
+ * @param {number} colsCount
+ * @return {Array<Array<number>>}
+ */
+Array.prototype.snail = function (rowsCount, colsCount) {
+    if (rowsCount * colsCount !== this.length) return [];
+
+    let output = Array.from({ length: rowsCount }, () =>
+        Array(colsCount).fill(0)
+    );
+    let traverse = -1;
+    let currentIndex = [0, -1];
+    for (let i = 0; i < this.length; i++) {
+        if (i % rowsCount === 0) {
+            currentIndex[1] += 1;
+            traverse *= -1;
+        } else {
+            currentIndex[0] += traverse;
+        }
+        output[currentIndex[0]][currentIndex[1]] = this[i];
+    }
+    return output;
+
+
+
+    
+}
+
+/**
+ * const arr = [1,2,3,4];
+ * arr.snail(1,4); // [[1,2,3,4]]
+ */
