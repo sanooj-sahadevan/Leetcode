850831b9909ca72f51b2f5377e7f05b683965bb3
sanooj <146158357+sanooj-sahadevan@users.noreply.github.com>
2025-01-17 11:26:28 +0530

    addd

--- 2624-difference-between-element-sum-and-digit-sum-of-an-array/difference-between-element-sum-and-digit-sum-of-an-array.js
+++ 2624-difference-between-element-sum-and-digit-sum-of-an-array/difference-between-element-sum-and-digit-sum-of-an-array.js
@@ -3,7 +3,12 @@
  * @return {number}
  */
 var differenceOfSum = function (nums) {
-    return Math.abs(nums.reduce((a, b) => a + b) - nums.join('').split('').map(v => Number(v)).reduce((acc, curr) => acc + curr))
+  return Math.abs(
+    nums.reduce((a, b) => a + b) -
+      nums
+        .join("")
+        .split("")
+        .map((v) => Number(v))
+        .reduce((acc, curr) => acc + curr)
+  );
 };
-
-
--- workout.js
+++ workout.js
@@ -1,29 +1,51 @@
-let head = [0, 3, 1, 0, 4, 5, 2, 0];
-// Output: [4,11]
-// Explanation:
-// The above figure represents the given linked list. The modified list contains
-// - The sum of the nodes marked in green: 3 + 1 = 4.
-// - The sum of the nodes marked in red: 4 + 5 + 2 = 11.
+let nums = [
+  19, 10, 3, 7, 9, 8, 5, 2, 1, 17, 16, 14, 12, 18, 6, 13, 11, 20, 4, 15,
+];
+rowsCount = 5;
+colsCount = 4;
+// Output:
+// [
+//  [19,17,16,15],
+//  [10,1,14,4],
+//  [3,2,12,20],
+//  [7,5,18,11],
+//  [9,8,6,13]
+// ]
 // Example 2:
 
-// Input: head = [0,1,0,3,0,2,2,0]
-// Output: [1,3,4]
-// Explanation:
-// The above figure represents the given linked list. The modified list contains
-// - The sum of the nodes marked in green: 1 = 1.
-// - The sum of the nodes marked in red: 3 = 3.
-// - The sum of the nodes marked in yellow: 2 + 2 = 4.
+// Input:
+// nums = [1,2,3,4]
+// rowsCount = 1
+// colsCount = 4
+// Output: [[1, 2, 3, 4]]
+// Example 3:
 
-var mergeNodes = function (head) {
+// Input:
+// nums = [1,3]
+// rowsCount = 2
+// colsCount = 2
+// Output: []
+// Explanation: 2 multiplied by 2 is 4, and the original array [1,3] has a length of 2; therefore, the input is invalid.
 
-  
-  let curr = this.head;
-  let sum = 0;
-  while (curr) {
-    if (curr.value != 0) {
-      sum += curr.value;
+Array.prototype.snail = function (rowsCount, colsCount) {
+  if (rowsCount * colsCount !== this.length) return [];
+
+  let output = Array.from({ length: rowsCount }, () =>
+    Array(colsCount).fill(0)
+  );
+  let traverse = -1;
+  let currentIndex = [0, -1];
+  for (let i = 0; i < this.length; i++) {
+    if (i % rowsCount === 0) {
+      currentIndex[1] += 1; 
+      traverse *= -1;
     } else {
-      sum = 0;
+      currentIndex[0] += traverse; 
     }
+    output[currentIndex[0]][currentIndex[1]] = this[i]; 
   }
+  return output;
 };
+
+const result = nums.snail(rowsCount, colsCount);
+console.log(result);
