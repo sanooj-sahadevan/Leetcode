cfbbd1ed6959fac818e8e3cc68ea58dca03e81f9
sanooj <146158357+sanooj-sahadevan@users.noreply.github.com>
2025-01-23 11:55:26 +0530

    addd

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -1,19 +1,18 @@
 var lengthOfLongestSubstring = function (s) {
-    let set = new Set();
-    let left = 0;
-    let maxSize = 0;
+  let set = new Set();
+  let left = 0;
+  let maxSize = 0;
 
-    if (s.length === 0) return 0;
-    if (s.length === 1) return 1;
+  if (s.length === 0) return 0;
+  if (s.length === 1) return 1;
 
-    for (let i = 0; i < s.length; i++) {
-
-        while (set.has(s[i])) {
-            set.delete(s[left])
-            left++;
-        }
-        set.add(s[i]);
-        maxSize = Math.max(maxSize, i - left + 1)
+  for (let i = 0; i < s.length; i++) {
+    while (set.has(s[i])) {
+      set.delete(s[left]);
+      left++;
     }
-    return maxSize;
-}
+    set.add(s[i]);
+    maxSize = Math.max(maxSize, i - left + 1);
+  }
+  return maxSize;
+};
--- workout.js
+++ workout.js
@@ -1,51 +1,20 @@
-let nums = [
-  19, 10, 3, 7, 9, 8, 5, 2, 1, 17, 16, 14, 12, 18, 6, 13, 11, 20, 4, 15,
-];
-rowsCount = 5;
-colsCount = 4;
-// Output:
-// [
-//  [19,17,16,15],
-//  [10,1,14,4],
-//  [3,2,12,20],
-//  [7,5,18,11],
-//  [9,8,6,13]
-// ]
+// let nums = [1, 2, 3, 4, 5, 6, 7],
+//   k = 3;
+// Output: [5,6,7,1,2,3,4]
+// Explanation:
+// rotate 1 steps to the right: [7,1,2,3,4,5,6]
+// rotate 2 steps to the right: [6,7,1,2,3,4,5]
+// rotate 3 steps to the right: [5,6,7,1,2,3,4]
 // Example 2:
 
-// Input:
-// nums = [1,2,3,4]
-// rowsCount = 1
-// colsCount = 4
-// Output: [[1, 2, 3, 4]]
-// Example 3:
+ Input: nums = [-1,-100,3,99], k = 2
+// Output: [3,99,-1,-100]
+// Explanation:
+// rotate 1 steps to the right: [99,-1,-100,3]
+// rotate 2 steps to the right: [3,99,-1,-100]
 
-// Input:
-// nums = [1,3]
-// rowsCount = 2
-// colsCount = 2
-// Output: []
-// Explanation: 2 multiplied by 2 is 4, and the original array [1,3] has a length of 2; therefore, the input is invalid.
-
-Array.prototype.snail = function (rowsCount, colsCount) {
-  if (rowsCount * colsCount !== this.length) return [];
-
-  let output = Array.from({ length: rowsCount }, () =>
-    Array(colsCount).fill(0)
-  );
-  let traverse = -1;
-  let currentIndex = [0, -1];
-  for (let i = 0; i < this.length; i++) {
-    if (i % rowsCount === 0) {
-      currentIndex[1] += 1; 
-      traverse *= -1;
-    } else {
-      currentIndex[0] += traverse; 
-    }
-    output[currentIndex[0]][currentIndex[1]] = this[i]; 
-  }
-  return output;
-};
-
-const result = nums.snail(rowsCount, colsCount);
-console.log(result);
+for (let i = 0; i < k; i++) {
+  let res = nums.pop();
+  nums.unshift(res);
+}
+console.log(nums);
