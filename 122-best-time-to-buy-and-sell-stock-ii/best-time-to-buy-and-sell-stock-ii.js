06abb8dbd8859c153ef6383a5ab7b7e0fc7f18ca
Sanooj Sahadevan <146158357+sanooj-sahadevan@users.noreply.github.com>
2025-01-24 12:05:43 +0530

    Time: 1 ms (71.50%) | Memory: 49.6 MB (46.26%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.js
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.js
@@ -2,20 +2,14 @@
  * @param {number[]} prices
  * @return {number}
  */
-var maxProfit = function (prices) {
+var maxProfit = function(prices) {
     let res = [];
+for (i = 0; i < prices.length; i++) {
+  if (prices[i] < prices[i + 1]) {
+    res.push(Math.abs(prices[i] - prices[i + 1]));
+  }
+}
+return res.reduce((acc,curr)=>acc+curr,0)
 
-    for (i = 0; i < prices.length; i++) {
-        if (prices[i] < prices[i + 1]) {
-            res.push(Math.abs(prices[i] - prices[i + 1]));
-        }
-    }
-    let sum = 0;
-    for (i = 0; i < res.length; i++) {
-        sum += res[i];
-    }
-    return sum
-    console.log(sum);
-
-
+    
 };
