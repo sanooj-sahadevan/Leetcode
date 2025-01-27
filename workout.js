aff3ad5f11297d7a866d2f99379dcb7507db2456
sanooj <146158357+sanooj-sahadevan@users.noreply.github.com>
2025-01-27 12:06:55 +0530

    addd

--- workout.js
+++ workout.js
@@ -1,17 +1,37 @@
-//let nums = [2, 3, 1, 1, 4];
-// Output: true
-// Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
-// Example 2:
+let num = 3749;
 
-Input: nums = [3,2,1,0,4]
-// Output: false
-// Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.
+// Output: "MMMDCCXLIX"
+// Explanation:
+// 3000 = MMM as 1000 (M) + 1000 (M) + 1000 (M)
+//  700 = DCC as 500 (D) + 100 (C) + 100 (C)
+//   40 = XL as 10 (X) less of 50 (L)
+//    9 = IX as 1 (I) less of 10 (X)
+// Note: 49 is not 1 (I) less of 50 (L) because the conversion is based on decimal places
 
-let sum = 0
-for (i = 0; i < nums.length; i++) {
-sum += nums[i]
-console.log(sum);
-//console.log(nums.length,'-----');
+let romanMap = {
+  M: 1000,
+  CM: 900,
+  D: 500,
+  CD: 400,
+  C: 100,
+  XC: 90,
+  L: 50,
+  XL: 40,
+  X: 10,
+  IX: 9,
+  V: 5,
+  IV: 4,
+  I: 1,
+};
 
-if(sum==nums.length-1) return true
+let result = "";
+for (let key in romanMap) {
+  while (num >= romanMap[key]) {
+    console.log(num);
+    //console.log(romanMap[key]);
+    result += key;
+    num -= romanMap[key];
+  }
 }
+console.log(result);
+ 
