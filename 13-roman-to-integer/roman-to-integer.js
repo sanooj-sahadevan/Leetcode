17352da580feee843435cf009b0f49123b2c1b06
Sanooj Sahadevan <146158357+sanooj-sahadevan@users.noreply.github.com>
2025-01-27 11:36:59 +0530

    Time: 106 ms (5.00%) | Memory: 58.5 MB (5.97%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+let f = {
+  I: 1,
+  V: 5,
+  X: 10,
+  L: 50,
+  C: 100,
+  D: 500,
+  M: 1000,
+};
+
+s = s.split("");
+let res = 0;
+for (i = s.length - 1; i >= 0; i--) {
+  if (f[s[i]] < f[s[i + 1]] || 0) {
+    res -= f[s[i]];
+    console.log(res);
+  } else {
+    res += f[s[i]];
+    console.log(res);
+  }
+}
+return res
+console.log(res);
+    
+};
