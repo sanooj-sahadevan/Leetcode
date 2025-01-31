ea8c0e34e76666d020d1ec218cb1d326829c0472
sanooj <146158357+sanooj-sahadevan@users.noreply.github.com>
2025-01-31 21:57:56 +0530

    addd

--- workout.js
+++ workout.js
@@ -1,22 +1,32 @@
-/**
- * @param {number[]} nums
- * @return {number[]}
- */
-var sortArrayByParity = function(nums) {
+// let s = "Hello World";
+// Output: 5
+// Explanation: The last word is "World" with length 5.
+// Example 2:
+
+// Input: s = "   fly me   to   the moon  ";
+// Output: 4
+// Explanation: The last word is "moon" with length 4.
+// Example 3:
+
+Input: s = "a";
+// Output: 6
+// Explanation: The last word is "joyboy" with length 6.
+
+let x = [];
+let ctr = 0;
+for (let i = s.length - 1; i >= 0; i--) {
+  console.log(s[i]);
+  x.push(s[i]);
+  if (s[i] == " " && ctr == 0) continue;
+  if (s[i] == " ") {
+    console.log('break');
     
-    let  x  = []
-    let  y  = []
-    
-    
-    nums.sort()
-    for(i=0;i<nums.length;i++){
-        if(nums[i]%2==0){
-      x.push(nums[i])
-        }else{
-            y.push(nums[i])
-        }
-    }
-    let a = [...x,...y]
-    return a
-   
-    };
+    break;
+  }
+  console.log("dd");
+
+  ctr++;
+}
+
+console.log(x.length - 1);
+console.log(ctr);
