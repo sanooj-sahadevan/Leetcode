e63c6f27695eb5c3cedbe75cfd6f3ffdfe5eeb54
sanooj <146158357+sanooj-sahadevan@users.noreply.github.com>
2025-02-03 13:03:18 +0530

    addd

--- workout.js
+++ workout.js
@@ -1,47 +1,46 @@
-// let s = "Hello World";
-// Output: 5
-// Explanation: The last word is "World" with length 5.
+Input: nums = [1, 4, 3, 3, 2];
+
+// Output: 2
+
+// The strictly increasing subarrays of nums are [1], [2], [3], [3], [4], and [1,4].
+// The strictly decreasing subarrays of nums are [1], [2], [3], [3], [4], [3,2], and [4,3].
+// Hence, we return 2.
 // Example 2:
-
-// Input: s = "   fly me   to   the moon  ";
-// Output: 4
-// Explanation: The last word is "moon" with length 4.
+// Input: nums = [3,3,3,3]
+// Output: 1
+// Explanation:
+// The strictly increasing subarrays of nums are [3], [3], [3], and [3].
+// The strictly decreasing subarrays of nums are [3], [3], [3], and [3].
+// Hence, we return 1.
 // Example 3:
+//Input: nums = [3, 2, 1];
+// Output: 3
+// Explanation:
+// The strictly increasing subarrays of nums are [3], [2], and [1].
+// The strictly decreasing subarrays of nums are [3], [2], [1], [3,2], [2,1], and [3,2,1].
 
-Input: s = "a";
-// Output: 6
-// Explanation: The last word is "joyboy" with length 6.
+let ctr = 1,
+  res = [];
+// for (let i = 0; i < nums.length; i++) {
+//   if (nums[i] < nums[i + 1]) {
+//     ctr++;
+//   }
+//   if (nums[i] > nums[i + 1]) {
+//     ctr = 1;
+//   }
+//   res.push(ctr);
+// }
+let count = 1;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-let x = [];
-let ctr = 0;
-for (let i = s.length - 1; i >=7 0; i--) {
-  console.log(s[i]);
-  x.push(s[i]);
-  if (s[i] == " " && ctr == 0) continue;
-  if (s[i] == " ") {
-    console.log('break');
+for (let i = nums.length - 1; i >= 0; i--) {
+  nums[i]
+  if (nums[i] > nums[i + 1]) {
     
-    break;
+    count++;
   }
-  console.log("dd");
-
-  ctr++;
+  if (nums[i] < nums[i + 1]) {
+    count = 1;
+  }
+  res.push(count);
 }
-
-console.log(x.length - 1);
-console.log(ctr);
+console.log(res);
