6fba4a980d95694fb975036e99a7e0ebb8513b77
sanooj <146158357+sanooj-sahadevan@users.noreply.github.com>
2025-02-06 20:59:48 +0530

    addd

--- workout.js
+++ workout.js
@@ -1,46 +1,14 @@
-Input: nums = [1, 4, 3, 3, 2];
-
-// Output: 2
-
-// The strictly increasing subarrays of nums are [1], [2], [3], [3], [4], and [1,4].
-// The strictly decreasing subarrays of nums are [1], [2], [3], [3], [4], [3,2], and [4,3].
-// Hence, we return 2.
+//let s = "the sky is blue";
+// Output: "blue is sky the"
 // Example 2:
-// Input: nums = [3,3,3,3]
-// Output: 1
-// Explanation:
-// The strictly increasing subarrays of nums are [3], [3], [3], and [3].
-// The strictly decreasing subarrays of nums are [3], [3], [3], and [3].
-// Hence, we return 1.
+
+ //Input: s = "  hello world  "
+// Output: "world hello"
+// Explanation: Your reversed string should not contain leading or trailing spaces.
 // Example 3:
-//Input: nums = [3, 2, 1];
-// Output: 3
-// Explanation:
-// The strictly increasing subarrays of nums are [3], [2], and [1].
-// The strictly decreasing subarrays of nums are [3], [2], [1], [3,2], [2,1], and [3,2,1].
 
-let ctr = 1,
-  res = [];
-// for (let i = 0; i < nums.length; i++) {
-//   if (nums[i] < nums[i + 1]) {
-//     ctr++;
-//   }
-//   if (nums[i] > nums[i + 1]) {
-//     ctr = 1;
-//   }
-//   res.push(ctr);
-// }
-let count = 1;
-
-for (let i = nums.length - 1; i >= 0; i--) {
-  nums[i]
-  if (nums[i] > nums[i + 1]) {
-    
-    count++;
-  }
-  if (nums[i] < nums[i + 1]) {
-    count = 1;
-  }
-  res.push(count);
-}
-console.log(res);
+Input: s = "a good   example"
+// Output: "example good a"
+// Explanation: You need to reduce multiple spaces between two words to a single space in the reversed string.
+ s = s.trim().split(/\s+/)
+console.log(s.reverse().join(' '));
