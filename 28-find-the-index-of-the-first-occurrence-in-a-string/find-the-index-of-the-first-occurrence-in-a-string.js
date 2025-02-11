c7ca7747425a826026f0a73d49d686bcb7dc0ffb
Sanooj Sahadevan <146158357+sanooj-sahadevan@users.noreply.github.com>
2025-02-11 12:33:29 +0530

    Time: 0 ms (100.00%) | Memory: 49.2 MB (37.05%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.js
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.js
@@ -3,31 +3,10 @@
  * @param {string} needle
  * @return {number}
  */
-var strStr = function (haystack, needle) {
-    function findFrom(i) {
-        if (haystack[i] === undefined) return -1;
-        if (haystack[i] === needle[0]) {
-            const initial = i;
-            i++;
-            let next = -1; 
-            let j = 1;
-            let matches = true;
-            while (needle[j]) {
-                if (haystack[i] === needle[0] && next === -1) next = i;
-                if (needle[j] !== haystack[i]) {
-                    if (next !== -1) return findFrom(next);
-                    matches = false;
-                    break;
-                }
-                i++;
-                j++;
-            }
+var strStr = function(haystack, needle) {
 
-            if (matches) return initial;
-            return findFrom(i);
-        }
-        return findFrom(++i);
-    }
-
-    return findFrom(0);
+    let index = haystack.indexOf(needle); // returns 6
+    return index
+console.log(index);
+    
 };
