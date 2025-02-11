e63cc3882c0c0706c03305d20067f71c7b01dedf
Sanooj Sahadevan <146158357+sanooj-sahadevan@users.noreply.github.com>
2025-02-11 12:59:46 +0530

    Time: 0 ms (100.00%) | Memory: 48.4 MB (99.25%) - LeetSync

---
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} s
+ * @param {string} part
+ * @return {string}
+ */
+var removeOccurrences = function(s, part) {
+     while (s.includes(part)) {
+        s = s.replace(part, "");
+    }
+    return s;
+    
+};
