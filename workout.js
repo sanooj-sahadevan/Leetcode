49d37972b9796bdc3eb8132f0c8365fa4e6dd3e0
sanooj <146158357+sanooj-sahadevan@users.noreply.github.com>
2025-02-12 12:26:03 +0530

    addd

--- workout.js
+++ workout.js
@@ -1,24 +1,11 @@
-let s = "daabcbaabcbc",
-  part = "abc";
-// Output: "dab"
-// Explanation: The following operations are done:
-// - s = "daabcbaabcbc", remove "abc" starting at index 2, so s = "dabaabcbc".
-// - s = "dabaabcbc", remove "abc" starting at index 4, so s = "dababc".
-// - s = "dababc", remove "abc" starting at index 3, so s = "dab".
-// Now s has no occurrences of "abc".
+//let s = "A man, a plan, a canal: Panama";
+// Output: true
+// Explanation: "amanaplanacanalpanama" is a palindrome.
 // Example 2:
+Input: s = "race a car";
+// Output: false
+// Explanation: "raceacar" is not a palindrome.
 
-// Input: s = "axxxxyyyyb", part = "xy"
-// Output: "ab"
-// Explanation: The following operations are done:
-// - s = "axxxxyyyyb", remove "xy" starting at index 4 so s = "axxxyyyb".
-// - s = "axxxyyyb", remove "xy" starting at index 3 so s = "axxyyb".
-// - s = "axxyyb", remove "xy" starting at index 2 so s = "axyb".
-// - s = "axyb", remove "xy" starting at index 1 so s = "ab".
-// Now s has no occurrences of "xy".
-
-
-while(s.includes(part)){
-  s.replace(part,'')
-  
-}
+var word = s.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+console.log(word.split(" ").reverse().join("") ===word);
+console.log(word);
