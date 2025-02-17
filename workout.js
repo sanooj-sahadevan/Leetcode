68fe9e96fdcd0da7f6c880e2a416049f4d2573cd
sanooj <146158357+sanooj-sahadevan@users.noreply.github.com>
2025-02-17 12:18:47 +0530

    addd

--- workout.js
+++ workout.js
@@ -1,18 +1,22 @@
-let s = "abc",
-  t = "ahbgdc";
-// Output: true
+let tiles = "AAB";
+// Output: 8
+// Explanation: The possible sequences are "A", "B", "AA", "AB", "BA", "AAB", "ABA", "BAA".
 
-let i = 0;
-for (let char of t) {
-  if (char === s[i]) i++;
-  if (i === s.length) break;
-}
+tiles = tiles.split("").sort().join("");
+let used = new Array(tiles.length).fill(false);
 
+const backtrack = () => {
+  let count = 0;
+  for (let i = 0; i < tiles.length; i++) {
+    if (used[i] || (i > 0 && tiles[i] === tiles[i - 1] && !used[i - 1]))
+      continue;
+    used[i] = true;
+    count += 1 + backtrack();
+    used[i] = false;
+  }
+  console.log(count);
 
+  return count;
+};
 
-
-
-
-
-
-console.log(i === s.length);
+backtrack()
