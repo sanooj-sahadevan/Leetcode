c51004a1cb39401c9060614bc3f308ec16968c77
sanooj <146158357+sanooj-sahadevan@users.noreply.github.com>
2025-03-01 21:15:09 +0530

    add

--- workout.js
+++ workout.js
@@ -1,1 +1,30 @@
-// hyut5ry6tgh
+Input: (pattern = "abab"), (s = "dog cat cat dog");
+
+//. Output: true
+// Explanation:
+// The bijection can be established as:
+// 'a' maps to "dog".
+// 'b' maps to "cat".
+// Example 2:
+// Input: pattern = "abba", s = "dog cat cat fish"
+// Output: false
+// Example 3:
+// Input: pattern = "aaaa", s = "dog cat cat dog"
+// Output: false
+
+pattern = pattern.split("");
+let f = {},
+  g = {};
+for (let i = 0; i < pattern.length; i++) {
+  f[pattern[i]] = (f[pattern[i]] || 0) + 1;
+}
+console.log(f);
+s = s.split(" ");
+
+for (let i = 0; i < s.length; i++) {
+  g[s[i]] = (g[s[i]] || 0) + 1;
+}
+console.log(
+  Object.values(f).map(JSON.stringify).join() ===
+    Object.values(g).map(JSON.stringify).join()
+);
