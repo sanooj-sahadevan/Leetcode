6470edd7c954a96e7d27e0f638fada22199c55d9
sanooj <146158357+sanooj-sahadevan@users.noreply.github.com>
2025-03-06 21:49:43 +0530

    add

---
+++ workout.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function (x) {
+    start = 0;
+    end = x / 2;
+    let y = 0;
+    while (start <= end) {
+      const mid = Math.floor((start + end) / 2);
+      if (mid * mid <= x) {
+        y = mid;
+        start = mid + 1;
+      } else {
+        end = mid - 1;
+      }
+    }
+    return -1;
+  };
+  
